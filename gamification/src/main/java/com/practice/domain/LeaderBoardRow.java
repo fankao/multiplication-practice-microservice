7c575aaf973e2b50f6517dd3cbf845d66e974285
fankao <minhchiendc@gmail.com>
2021-01-24 14:54:34 +0700

    Create domains for gamification service

---
+++ gamification/src/main/java/com/practice/domain/LeaderBoardRow.java
@@ -0,0 +1,22 @@
+package com.practice.domain;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Value;
+import lombok.With;
+
+@Value
+@AllArgsConstructor
+public class LeaderBoardRow {
+	Long userId;
+	Long totalScore;
+	@With
+	List<String> badges;
+
+	public LeaderBoardRow(final Long userId, final Long totalScore) {
+		this.userId = userId;
+		this.totalScore = totalScore;
+		this.badges = List.of();
+	}
+}
