32416bd0c57ee9503b90555d751e42dc58736e1b
fankao <minhchiendc@gmail.com>
2021-01-24 14:58:34 +0700

    Structure project into microservices

--- src/main/java/com/practice/challenge/ChallengeGeneratorService.java
+++
@@ -1,9 +0,0 @@
-package com.practice.challenge;
-
-public interface ChallengeGeneratorService {
-	/**
-	 * 
-	 * @return a randomly-generated with factors between 11 and 99
-	 */
-	Challenge randomChallenge();
-}
